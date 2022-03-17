efc81cc0b382bfb16f170f3bb6f60c23d747422c
Edmund R. R. Moody <edmund93@gmail.com>
2022-03-17 10:35:10 +0000

    Find proteins using HMMER3 output table, from a fasta file

---
+++ seq_from_hmm.py
@@ -0,0 +1,25 @@
+from Bio import SeqIO
+from Bio import SearchIO
+import sys
+
+hit_ids = []
+sequences = []
+
+
+with open (sys.argv[1], 'r') as input:
+	for result in SearchIO.parse(input, 'hmmsearch3-domtab'):
+		hits = result.hits
+		num_hits = len(hits)	
+		for x in range(num_hits):
+			hit_ids.append(hits[x].id)
+
+
+for record in SeqIO.parse(sys.argv[2], "fasta"):
+	if record.id in hit_ids:
+		sequences.append(record)
+
+
+output = str(sys.argv[1]) + '.arc_euk_added'
+
+SeqIO.write(sequences, output, "fasta")
+
