0064c6f6fb892060e1bc62a8cee3c45dd5d406d1
Edmund R. R. Moody <edmund93@gmail.com>
2025-04-09 17:02:03 +0100

    Update remove_from_list_v2.py

--- remove_from_list_v2.py
+++ remove_from_list_v2.py
@@ -3,19 +3,20 @@ import sys
 filename = sys.argv[1]
 listofspecies = []
 
-listfile = open(sys.argv[2], 'r')
-for line in listfile:
-    x = line.rstrip()
-    listofspecies.append(x)
+with open(sys.argv[2], 'r') as listfile:
+    for line in listfile:
+        x = line.rstrip()
+        listofspecies.append(x)
 
 with open(f"{filename}.filtered", "w") as outfile:
     with open(filename, "r") as infile:
         skip = False
         for line in infile:
-            if any(species in line for species in listofspecies):
-                skip = True
-            elif skip:
-                if line.startswith(">"):
+            if line.startswith(">"):
+                # Check if any species is in the header line
+                if any(species in line for species in listofspecies):
+                    skip = True
+                else:
                     skip = False
-            else:
+            if not skip:
                 outfile.write(line)
